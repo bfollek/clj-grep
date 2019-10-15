a94cd82ccad04ad70b56821bb214b13f7b9ccdb5
Bob Follek <bfollek@gmail.com>
2019-10-15 06:37:55 -0400

    First test ATP

--- test/clj_grep/core_test.clj
+++ test/clj_grep/core_test.clj
@@ -2,6 +2,11 @@
   (:require [clojure.test :refer :all]
             [clj-grep.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-load-state
+  (is (= {:files ["mike.txt"]
+          :options {:entire-lines true
+                    :ignore-case false
+                    :invert false
+                    :line-numbers true
+                    :only-names false}
+          :pattern "foo\n"} (load-state "foo" "-x -n" ["mike.txt"]))))
