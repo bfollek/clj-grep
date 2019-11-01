f10c01a2f7cdbfecac7ba1a29578d264031cd8a5
Bob Follek <bfollek@gmail.com>
2019-11-01 08:39:40 -0400

    BF WIP ATP Line numbers in there, but not used yet.

--- src/clj_grep/core.clj
+++ src/clj_grep/core.clj
@@ -1,8 +1,8 @@
 (ns clj-grep.core
   "grep"
-  (:require [clojure.string :as str]
-            [clojure.tools.cli :as cli]
-            [rabbithole.core :as rh]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.tools.cli :as cli]))
 
 (defrecord Options [entire-lines ignore-case invert line-numbers only-names])
 (defrecord State [pattern options file-names])
@@ -40,20 +40,29 @@
         pattern (load-pattern pattern options)]
     (map->State {:pattern pattern :options options :file-names file-names})))
 
+(defn- number-lines
+  "Transform a sequence of lines into a sequence of vectors.
+  Each vector has a line number and a line."
+  ; clj-grep.core=> (number-lines '("aaa" "bbb" "ccc"))
+  ; ([1 "aaa"] [2 "bbb"] [3 "ccc"])
+  [lines]
+  (map-indexed (fn [idx elem] [(inc idx) elem]) lines))
+
 (defn- matches?
   [state line]
-  (let [line (cond-> line (get-option state :ignore-case) str/lower-case)
+  (let [txt (cond-> (second line) (get-option state :ignore-case) str/lower-case)
         match-func (if (get-option state :entire-lines) = str/includes?)
-        match? (match-func line (:pattern state))]
+        match? (match-func txt (:pattern state))]
     (cond-> match? (get-option state :invert) not)))
 
 (defn- fmt-line
   [line file-name state]
-  (let [options (:options state)]
+  (let [txt (second line)
+        options (:options state)]
     (cond
       (:only-names options) file-name
-      (> (count (:file-names state)) 1) (format "%s:%s" file-name line)
-      :else line)))
+      (> (count (:file-names state)) 1) (format "%s:%s" file-name txt)
+      :else txt)))
 
 (defn- fix-path
   [file-name]
@@ -61,19 +70,21 @@
 
 (defn- check-file
   [state file-name]
-  (->> (rh/read-lines (fix-path file-name))
-       (filter #(matches? state %))
-       (map #(fmt-line % file-name state))))
+  (with-open [rdr (io/reader (fix-path file-name))]
+    (doall (->> (line-seq rdr)
+                number-lines
+                (filter #(matches? state %))
+                (map #(fmt-line % file-name state))))))
 
-(defn run
+(defn- run
   [state]
   (-> (map #(check-file state %) (:file-names state))
       flatten
       ; If only file names, remove duplicates.
       (cond-> (get-option state :only-names) distinct)
-      ; Add a newlines
+      ; Add newlines.
       (interleave (repeat "\n"))
-      ; Return one big string
+      ; Return one big string.
       str/join))
 
 (defn grep
